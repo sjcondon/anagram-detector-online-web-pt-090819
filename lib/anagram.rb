68317604ebd245c35a710b0353c12051593ea1ce
sarah <s.j.condon01@gmail.com>
2019-10-06 18:40:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ end
   def match(array)
     arr = []
       array.each do |anagrams|
-        if anagrams.split("") === @word.split("").sort
+        if anagrams.split("").sort === @word.split("").sort
           arr << anagrams
         end
       end

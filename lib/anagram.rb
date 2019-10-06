8d8e90a3d5b8d6bf71e3826b35df51a6d0f0bfc1
sarah <s.j.condon01@gmail.com>
2019-10-06 18:45:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word #setter
+  
+  def initialize(word)
+    @word = word #getter
+end
+  
+  def match(array) #iterate over the array
+    arr = []
+      array.each do |anagrams| #figure out if the are composed of the same letter. Going through each anagrams
+        if anagrams.split("").sort === @word.split("").sort #splitting up the anagrams and then sorting them. 
+                                                            #next checking to see if the anagrams are equal to the sorted words
+          arr << anagrams #shovel anagrams into the empty array
+        end
+      end
+      arr
+  end
+end
